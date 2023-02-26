a3a9ba4c5bdd9127ac3f03a085f11bc2fc2e4b65
justdealingdmg <99028194+justdealingdmg@users.noreply.github.com>
2023-02-26 22:32:02 +0200

    Add files via upload

---
+++ SERGIU ANDREI WEBSITE/js/init.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function() {
+						   
+	$(".countdown").countdown({
+				date: "1 January 2024 18:30:00",
+				format: "on"
+			},
+			
+			function() {
+				
+			});
+
+});	
